e8ff1a6c9462fdaa92638aaf262dffbe5ea0555f
code_red <72528267+Corters22@users.noreply.github.com>
2021-04-15 20:41:41 -0500

    started demo info box

--- static/js/app.js
+++ static/js/app.js
@@ -2,6 +2,8 @@ const path = '../samples.json';
 
 // Step 1: Plotly
 
+function init() {
+
 // Use the D3 library to read in samples.json.
 d3.json(path).then(function(data) {
     var fullData = data;
@@ -11,14 +13,14 @@ d3.json(path).then(function(data) {
     var otu_ids = samples.map(sample => sample.otu_ids);
     var sampleValues = samples.map(sample => sample.sample_values);
     var otuLabels = samples.map(sample => sample.otu_labels);
-    console.log("otu id for first item", otu_ids.slice(1, 10));
+    console.log("meta for 1st pt", meta[0]);
 
     //Create a horizontal bar chart with a dropdown menu to display the 
     // top 10 OTUs found in that individual.
 
     var data = [{
         y: otu_ids[0].slice(0, 10).map((id) => {
-            return `OTU${id}`}),
+            return `OTU ${id}`}),
         x: sampleValues[0].slice(0, 10),
         type: "bar",
         orientation: "h"
@@ -30,12 +32,20 @@ d3.json(path).then(function(data) {
             },
         }
     
-
     function barPlot(){
         Plotly.newPlot('bar', data, layout);
     }
     barPlot();
-});
 
+    function buildDemo() {
+        var demoInfo = d3.select('#sample-metadata');
+        
+    }
 
+    d3.select("#sample-metadata").text(Object.entries(meta[0]))
+})};
 
+init();
+
+//dropdown value === data.samples[0].id - for the charts
+// === data.metadata.id, maybe use unpack
